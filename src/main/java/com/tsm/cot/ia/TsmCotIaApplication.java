3b4b9296b64a23d7f1bdb10ac1b83f462aa28cbe
SimonePiccirilli <simone.piccirilli91@gmail.com>
2025-02-13 13:17:49 +0100

    first commit

---
+++ src/main/java/com/tsm/cot/ia/TsmCotIaApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.cot.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmCotIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmCotIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsm/cot/ia/TsmCotIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.cot.ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsmCotIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
